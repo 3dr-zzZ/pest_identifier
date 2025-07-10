4c939fe164fcbe73b1f6cdee418289f900a9e372
3dr-zzZ <508874905@qq.com>
2025-07-10 16:56:47 +0800

    modified resize

--- predict/predict.py
+++ predict/predict.py
@@ -49,8 +49,7 @@ def get_transforms() -> transforms.Compose:
     """Return the same preprocessing pipeline used during training."""
     return transforms.Compose(
         [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(
                 mean=[0.485, 0.456, 0.406],
