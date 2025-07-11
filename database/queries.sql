91fe2ee4a23e6261e73608ccf2d2e15046ec623d
3dr-zzZ <508874905@qq.com>
2025-07-11 14:37:07 +0800

    removed double quotes, added new queries

--- database/queries.sql
+++ database/queries.sql
@@ -1,17 +1,24 @@
+-- to see what species are included in the database, use:
+SELECT scientific_name FROM species;
+
+-- to access a species' information, use:
+SELECT chinese_name, other_name, traits FROM species
+WHERE scientific_name = 'Aedes albopictus';
+
 -- to access a species' distributed locations, use:
-SELECT "scientific_name", "locations"."name" FROM "species"
-JOIN "distributed" ON "species"."id" = "distributed"."species_id"
-JOIN "locations" ON "distributed"."location_id" = "locations"."id"
-WHERE "scientific_name" = 'Aedes albopictus';
+SELECT locations.name, locations.type FROM species
+JOIN distributed ON species.id = distributed.species_id
+JOIN locations ON distributed.location_id = locations.id
+WHERE scientific_name = 'Aedes albopictus';
 
 -- to access a species' taxonomy, use:
-SELECT "scientific_name", "taxonomies"."name", "taxonomies"."type" FROM "species"
-JOIN "belongs" ON "species"."id" = "belongs"."species_id"
-JOIN "taxonomies" ON "belongs"."taxonomy_id" = "taxonomies"."id"
-WHERE "scientific_name" = 'Aedes albopictus';
+SELECT taxonomies.name, taxonomies.chinese_name, taxonomies.type FROM species
+JOIN belongs ON species.id = belongs.species_id
+JOIN taxonomies ON belongs.taxonomy_id = taxonomies.id
+WHERE scientific_name = 'Aedes albopictus';
 
 -- to access a species' diseases carried, use:
-SELECT "scientific_name", "diseases"."name" FROM "species"
-JOIN "carries" ON "species"."id" = "carries"."species_id"
-JOIN "diseases" ON "carries"."disease_id" = "diseases"."id"
-WHERE "scientific_name" = 'Aedes albopictus';
+SELECT diseases.name FROM species
+JOIN carries ON species.id = carries.species_id
+JOIN diseases ON carries.disease_id = diseases.id
+WHERE scientific_name = 'Aedes albopictus';
